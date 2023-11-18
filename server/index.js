9532b6faaba1b3266e8011c5b66351428b4ef821
Apoorv Pandey <apoorvrpandey@gmail.com>
2023-11-18 22:53:29 +0530

    New Deployment

--- server/index.js
+++ server/index.js
@@ -28,10 +28,10 @@ mongoose
     console.log(err);
   });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.resolve("../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.resolve("../client/dist/index.html"));
 });
 
 app.use((err, req, res, next) => {
